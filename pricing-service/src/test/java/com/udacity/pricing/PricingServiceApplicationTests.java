5a29bb81ab215bad10957c3102545a04f8461f4d
yuxiRen <rita.ren.yuxi@gmail.com>
2020-10-26 23:34:43 -0700

    test pricing

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,18 +1,17 @@
 package com.udacity.pricing;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.udacity.pricing.api.PricingController;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(PriceController.class)
-class PriceclientApplicationTests {
+@WebMvcTest(PricingController.class)
+public class PricingServiceApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
